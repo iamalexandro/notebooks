10ecb74c310e346fb085394dcfd44bde94ad6996
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2019-11-04 15:11:40 -0500

    changes 4 of November

--- curso-angular/introduccion TypeScript y ES6/app.js
+++ curso-angular/introduccion TypeScript y ES6/app.js
@@ -1,10 +1,9 @@
-function saludar(nombre:String) {
+"use strict";
+function saludar(nombre) {
     console.log("Hola " + nombre.toUpperCase());
 }
 var wolverine = {
     nombre: "Logan"
 };
-
 //var num = 5;
-
 saludar(wolverine.nombre);
